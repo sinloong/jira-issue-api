e231f5066ff7ff1a60e3be61d0320c80225e5623
SinLoong <sinloong.koo@igloohome.co>
2022-10-18 19:39:02 +0800

    imitial commit

---
+++ GetHistoriesForOneTicket.js
@@ -0,0 +1,86 @@
+// This code sample uses the 'node-fetch' library:
+// https://www.npmjs.com/package/node-fetch
+import fetch from "node-fetch";
+import csvWriter from "csv-writer";
+
+class HTTPResponseError extends Error {
+  constructor(response, ...args) {
+    super(
+      `HTTP Error Response: ${response.status} ${response.statusText}`,
+      ...args
+    );
+    this.response = response;
+  }
+}
+
+const checkStatus = (response) => {
+  if (response.ok) {
+    // response.status >= 200 && response.status < 300
+    return response;
+  } else {
+    throw new HTTPResponseError(response);
+  }
+};
+
+const exportCSV = (histories) => {
+  const items = [];
+
+  histories.forEach((history) => {
+    //console.log(history.author.displayName);
+
+    history.items.forEach((item) => {
+      items.push({
+        id: history.id,
+        author: history.author.displayName,
+        field: item.field,
+        fromString: item.fromString,
+        toString: item.toString,
+        created: history.created,
+      });
+    });
+  });
+
+  csv
+    .writeRecords(items) // returns a promise
+    .then(() => {
+      console.log("...Done");
+    });
+  //console.log(output);
+};
+
+const createCSV = csvWriter.createObjectCsvWriter;
+const csv = createCSV({
+  path: "demoD.csv",
+  header: [
+    { id: "id", title: "ID" },
+    { id: "author", title: "Author" },
+    { id: "field", title: "Field" },
+    { id: "fromString", title: "FromString" },
+    { id: "toString", title: "ToString" },
+    { id: "created", title: "Created" },
+  ],
+});
+
+const response = await fetch(
+  "https://iglooapp.atlassian.net/rest/api/3/issue/DP-182/changelog",
+  {
+    method: "GET",
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        "sinloong.koo@igloohome.co:oZ5ULrvUXAz3nKeRNe432DBF"
+      ).toString("base64")}`,
+      Accept: "application/json",
+    },
+  }
+);
+
+try {
+  checkStatus(response);
+  const histories = await response.json();
+  exportCSV(histories.values);
+} catch (error) {
+  console.error(error);
+
+  const errorBody = await error.response.text();
+  console.error(`Error body: ${errorBody}`);
+}
