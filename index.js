0a20f3484b9aafcab3c3f83f17ce5556b8069b71
SinLoong <sinloong.koo@igloohome.co>
2022-10-19 14:32:47 +0800

    update reviewed column value from changelogs status 'Done'

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const fields = [
   { id: "fields.updated", title: "Updated" },
   { id: "fields.lastViewed", title: "Last Viewed" },
   { id: "fields.resolutiondate", title: "Resolved" },
+  { id: "fields.resolutiondatefromlastdone", title: "Resolved From Last Done" },
   { id: "fields.fixVersions", title: "Fix Versions" },
   { id: "fields.components", title: "Components" },
   { id: "fields.duedate", title: "Due Date" },
@@ -56,9 +57,6 @@ const fields = [
   { id: "fields.timeestimate", title: "Remaining Estimate" },
   { id: "fields.timespent", title: "Time Spent" },
   { id: "fields.workratio", title: "Work Ratio" },
-  
-  
-  
 ];
 
 const getFieldValue = (issue, fieldName) => {
@@ -70,19 +68,27 @@ const getFieldValue = (issue, fieldName) => {
     } else if (splitFieldName.length == 2) {
       returnValue = issue[splitFieldName[0]][splitFieldName[1]];
     } else if (splitFieldName.length == 3) {
-      returnValue = issue[splitFieldName[0]][splitFieldName[1]][splitFieldName[2]];
+      returnValue =
+        issue[splitFieldName[0]][splitFieldName[1]][splitFieldName[2]];
     }
 
     if (fieldName === "fields.description" && returnValue) {
       returnValue = getDescription(returnValue).join("\r\n");
     }
-    if ((fieldName === "fields.fixVersions" || fieldName === "fields.components")
-     && returnValue && returnValue.length > 0) {
-      returnValue = returnValue.map(v => v.name).join(",")
+    if (
+      (fieldName === "fields.fixVersions" ||
+        fieldName === "fields.components") &&
+      returnValue &&
+      returnValue.length > 0
+    ) {
+      returnValue = returnValue.map((v) => v.name).join(",");
     }
-    if ((fieldName === "fields.labels")
-     && returnValue && returnValue.length > 0) {
-      returnValue = returnValue.join(",")
+    if (
+      fieldName === "fields.labels" &&
+      returnValue &&
+      returnValue.length > 0
+    ) {
+      returnValue = returnValue.join(",");
     }
   } catch (e) {
     console.log("Failed to get " + fieldName + " field");
@@ -93,49 +99,80 @@ const getFieldValue = (issue, fieldName) => {
 //convert description json into text string only
 const getDescription = (obj) => {
   let textArray = [];
-  Object.keys(obj).forEach(key => {
+  Object.keys(obj).forEach((key) => {
+    //console.log(`key: ${key}, value: ${obj[key]}`)
 
-      //console.log(`key: ${key}, value: ${obj[key]}`)
-      
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-          const temp = getDescription(obj[key])
-          if (temp.length > 0) textArray = textArray.concat(temp)
-      }
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      const temp = getDescription(obj[key]);
+      if (temp.length > 0) textArray = textArray.concat(temp);
+    }
 
-      if (key === "text") {
-          textArray.push(obj[key])
-      } 
-      //console.log(textArray)
-  })
-  return textArray
-}
+    if (key === "text") {
+      textArray.push(obj[key]);
+    }
+    //console.log(textArray)
+  });
+  return textArray;
+};
 
-const exportCSV = (searchResult) => {
+function mapIssuesFields(searchResult) {
   const names = searchResult.names;
   const issuesData = searchResult.issues;
-  const issues = [];
 
-  issuesData.forEach((issue) => {
+  const issues = issuesData.map((issue) => {
     //console.log(history.author.displayName);
     let newIssue = {};
     fields.forEach((field) => {
       newIssue[field.id] = getFieldValue(issue, field.id);
     });
-    issues.push(newIssue);
+    return newIssue;
   });
 
-  csv
-    .writeRecords(issues) // returns a promise
-    .then(() => {
-      console.log("...Done");
-    });
-  //console.log(output);
+  return issues;
+}
+
+const fetchIssuesFromJira = async (jqlBodyData) => {
+  const response = await fetch(
+    "https://iglooapp.atlassian.net/rest/api/3/search",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          "sinloong.koo@igloohome.co:oZ5ULrvUXAz3nKeRNe432DBF"
+        ).toString("base64")}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: jqlBodyData,
+    }
+  );
+  return response;
 };
 
-const csv = csvWriter.createObjectCsvWriter({
-  path: "output/demoD.csv",
-  header: fields
-});
+const fetchLastDoneStatusUpdateFromJiraChangeLog = async (issueKey) => {
+  const response = await fetch(
+    "https://iglooapp.atlassian.net/rest/api/3/issue/" + issueKey + "/changelog",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          "sinloong.koo@igloohome.co:oZ5ULrvUXAz3nKeRNe432DBF"
+        ).toString("base64")}`,
+        Accept: "application/json",
+      },
+    }
+  );
+  const histories = await response.json()
+  let lastStatusDoneUpdate = null;
+  for (const history of histories.values) {
+    for (const item of history.items) {
+      if (item.fieldId === "status" && item.to === "10001") {
+        lastStatusDoneUpdate = history.created
+      }
+    }
+  }
+  return lastStatusDoneUpdate;
+};
 
 const bodyData = `{
     "expand": [
@@ -145,35 +182,52 @@ const bodyData = `{
         "changelog"
     ],
     "jql": "project in (igloohome-2022, iglooworks-2022, iDP, 'Igloohome Flutter', 'iglooworks App 2.0', 'iglooworks Dashboard 2.0', 'iglooworks app', 'iglooworks Dashboard', 'HW Product', 'CS Team Tool', 'CS Team Tool 2022', 'Bluetooth Mobile SDK', 'Aztech Bridge', 'IglooApp 2.0', iglooconnect-2022, Analytics, 'PRS App', 'PRS Admin App', 'Auto Test App') AND status in (Done, Resolved, Closed) AND createdDate >= 2022-01-01 AND createdDate < 2022-10-01 ORDER BY created DESC",
-    "maxResults": 1000,
+    "maxResults": 100,
     "fieldsByKeys": false,
     "fields": [
     ],
-    "startAt": 0
+    "startAt": {{startAt}}
 }`;
 
-const response = await fetch(
-  "https://iglooapp.atlassian.net/rest/api/3/search",
-  {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${Buffer.from(
-        "sinloong.koo@igloohome.co:oZ5ULrvUXAz3nKeRNe432DBF"
-      ).toString("base64")}`,
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: bodyData,
-  }
-);
+//"jql": "project in (igloohome-2022, iglooworks-2022, iDP, 'Igloohome Flutter', 'iglooworks App 2.0', 'iglooworks Dashboard 2.0', 'iglooworks app', 'iglooworks Dashboard', 'HW Product', 'CS Team Tool', 'CS Team Tool 2022', 'Bluetooth Mobile SDK', 'Aztech Bridge', 'IglooApp 2.0', iglooconnect-2022, Analytics, 'PRS App', 'PRS Admin App', 'Auto Test App') AND status in (Done, Resolved, Closed) AND createdDate >= 2022-01-01 AND createdDate < 2022-10-01 ORDER BY created DESC",
+//"jql": "project in ('iglooworks Dashboard') AND status in (Done, Resolved, Closed) AND createdDate >= 2022-01-01 AND createdDate < 2022-10-01 ORDER BY created DESC",
 
 try {
-  checkStatus(response);
-  const searchResult = await response.json();
-  exportCSV(searchResult);
+  const issues = [];
+  let index = 0;
+  let searchResult = []
+
+  do {
+    const response = await fetchIssuesFromJira(bodyData.replace('{{startAt}}', index));
+    checkStatus(response);
+    searchResult = await response.json();
+    issues.push(...mapIssuesFields(searchResult));
+    index += 100;
+    console.log("Processing records -> " + index)
+  } while (searchResult.issues.length > 0);
+
+  for (const issue of issues) {
+    if (issue["fields.status.name"] == "Done" && !issue["fields.resolutiondate"]) {
+      issue["fields.resolutiondate"] = await fetchLastDoneStatusUpdateFromJiraChangeLog(issue["key"]);
+      issue["fields.resolutiondatefromlastdone"] = "1"
+    }
+  }
+
+  const csv = csvWriter.createObjectCsvWriter({
+    path: "output/demoD.csv",
+    header: fields
+  });
+
+  csv
+    .writeRecords(issues) // returns a promise
+    .then(() => {
+      console.log("...Done");
+    });
 } catch (error) {
   console.error(error);
 
-  const errorBody = await error.response.text();
+  const errorBody = await error.message;
+  const errorStackTrace = await error.stack;
   console.error(`Error body: ${errorBody}`);
+  console.error(`Error StackTrace: ${errorStackTrace}`);
 }
